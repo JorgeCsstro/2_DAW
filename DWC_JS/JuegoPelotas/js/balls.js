85d8aca8434d4f5b0f4f2d1165f402e7d15f2439
JorgeCsstro <jorgecastrot2005@gmail.com>
2024-10-22 16:02:37 +0200

    JS: Empezando juego de las pelotas

---
+++ DWC_JS/JuegoPelotas/js/balls.js
@@ -0,0 +1,37 @@
+window.onload = init;
+function init(){
+    document.querySelector(".start").addEventListener("click",cronometrar);
+    document.querySelector(".stop").addEventListener("click",parar);
+    document.querySelector(".reiniciar").addEventListener("click",reiniciar);
+    m = 0;
+    s = 0;
+    document.getElementById("ms").innerHTML="00:00";
+    document.getElementById("buenas").innerHTML="0";
+    document.getElementById("malas").innerHTML="0";
+}         
+function cronometrar(){
+    escribir();
+    id = setInterval(escribir,1000);
+    document.querySelector(".start").removeEventListener("click",cronometrar);
+}
+function escribir(){
+    var mAux, sAux;
+    s++;
+    if (s>59){m++;s=0;}
+
+    if (s<10){sAux="0"+s;}else{sAux=s;}
+    if (m<10){mAux="0"+m;}else{mAux=m;}
+
+    document.getElementById("ms").innerHTML = mAux + ":" + sAux; 
+}
+function parar(){
+    clearInterval(id);
+    document.querySelector(".start").addEventListener("click",cronometrar);
+
+}
+function reiniciar(){
+    clearInterval(id);
+    document.getElementById("ms").innerHTML="00:00";
+    h=0;m=0;s=0;
+    document.querySelector(".start").addEventListener("click",cronometrar);
+}

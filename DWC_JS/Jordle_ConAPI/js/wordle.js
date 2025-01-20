7578e8ccd2f60e79d1407cfb08383b18ae375192
JorgeCsstro <jorgecastrot2005@gmail.com>
2025-01-20 20:31:21 +0100

    JS: Jordle API acabado (de verdad)

--- DWC_JS/Jordle_ConAPI/js/wordle.js
+++ DWC_JS/Jordle_ConAPI/js/wordle.js
@@ -182,7 +182,7 @@ async function apiPalabra() {
 
         palabraRand = removeAccents(palabraRand);
 
-        if (palabraRand.length < 10 || palabraRand.length > 3) {
+        if (palabraRand.length < 9 && palabraRand.length > 3) {
             if (await compruebaRAErandom(palabraRand) == true) {
                 encontrarValidacion = false;
             }

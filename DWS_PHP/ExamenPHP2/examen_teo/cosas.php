6a9d4ef5fd2b8565117dc89d8ed8794f6a374800
JorgeCsstro <jorgecastrot2005@gmail.com>
2025-01-28 19:55:46 +0100

    PHP: Examen teorico

---
+++ DWS_PHP/ExamenPHP2/examen_teo/cosas.php
@@ -0,0 +1,22 @@
+<?php
+
+    /*
+    
+    UD1:
+        La arquitectura cliente/servidor está basada en la idea de servicios:
+            El cliente solicita servicios (REQUEST)
+
+            El servidor es un proceso proveedor de dichos
+            servicios (RESPONSE)
+        
+        
+
+    */
+
+
+    $varN=2;
+    $varC="5 pueblos";
+    $varC=$varN;
+
+
+?>

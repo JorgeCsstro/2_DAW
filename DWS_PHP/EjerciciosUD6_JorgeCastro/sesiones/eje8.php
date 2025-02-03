1dff752add428c5ebd41aa15a305430268f6e807
JorgeCsstro <jorgecastrot2005@gmail.com>
2025-02-03 18:14:58 +0100

    JS: Empezando PokeVue con drag and dro

--- DWS_PHP/EjerciciosUD6_JorgeCastro/sesiones/eje8.php
+++ DWS_PHP/EjerciciosUD6_JorgeCastro/sesiones/eje8.php
@@ -1,14 +1,13 @@
 <?php
+session_start();
 /**
  * @author Jorge Castro <jorgecastrot2005@gmail.com>
  */
 
 /*
-    8.  Usa el formulario (Ejercicio 18 UD5) de cálculo de media, mediana y moda donde se indiquen
-        varios números y pueda seleccionar una o todas las opciones de cálculo sobre esos números y
-        las muestre guardando estos datos en una Cookie. Se deben mostrar los números con los
-        cálculos seleccionados en el presente y los números y los cálculos realizados en la ocasión
-        anterior (cookie).
+    8.  Usa el formulario del ejercicio 8 de Cookies con selección de zona horaria para mostrar la hora
+        y zona elegidas de modo que uses la sesión para mostrar la zona horaria y hora actuales y
+        además muestre la zona horaria y hora de la selección anterior.
 */
 
 // Calcular media
@@ -51,24 +50,21 @@ if (isset($_GET['enviar'])) {
     }
     if (in_array('moda', $opciones)) {
         $moda = calcularModa($numeros);
-        $stringDatos = $stringDatos . "Moda: " . implode(', ', $moda) . ",";
+        $stringDatos .= "Moda: " . implode(', ', $moda) . ",";
     }
     if (in_array('mediana', $opciones)) {
         $mediana = calcularMediana($numeros);
-        $stringDatos = $stringDatos . "Mediana: $mediana";
+        $stringDatos .= "Mediana: $mediana";
     }
 
-    // Me quedo las operaciones en un array
-    $array_datos_actuales = explode(",", $stringDatos);
-
-    // Creo una cookie con las operaciones
-    setcookie("datosAnt", $stringDatos, time() + (24 * 60 * 60), "/");
-
-    // Cojo los datos y los pongo en un array
-    $datosCookie = $_COOKIE["datosAnt"];
-    $array_datos_cookie = explode(",", $datosCookie);
-    
+    // Guardar en sesión
+    $_SESSION['datosAnt'] = $_SESSION['datosActuales'] ?? '';
+    $_SESSION['datosActuales'] = $stringDatos;
 }
+
+// Obtener datos de la sesión
+$datosActuales = $_SESSION['datosActuales'] ?? 'No hay cálculos actuales';
+$datosAnteriores = $_SESSION['datosAnt'] ?? 'No hay cálculos anteriores';
 ?>
 
 <html lang="es">
@@ -97,16 +93,9 @@ if (isset($_GET['enviar'])) {
 
 <!--Imprimo todos los datos-->
 <h2>Datos Actuales:</h2>
-    <?php 
-        for ($i=0; $i < count($array_datos_actuales); $i++) { 
-            print("<p>" . $array_datos_actuales[$i] . "</p>");
-        }
-    ?>
-    <h2>Datos Anteriores:</h2>
-    <?php 
-        for ($i=0; $i < count($array_datos_cookie); $i++) { 
-            print("<p>" . $array_datos_cookie[$i] . "</p>");
-        }
-    ?>
+<p><?php echo $datosActuales; ?></p>
+
+<h2>Datos Anteriores:</h2>
+<p><?php echo $datosAnteriores; ?></p>
 </body>
 </html>
